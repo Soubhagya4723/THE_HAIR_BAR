fbdddd5858ca02eb172946209b3bb8f0b3a3cfed
soubhagya <soubhagyakumar373@gmail.com>
2026-05-31 11:48:17 +0530

    FIX

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -62,7 +62,7 @@ export default function HeroSection() {
                   fontFamily: "Archivo Black, sans-serif",
                 }}
               >
-                HAIR
+                HAIRBAR
               </h1>
 
               {/* Subtitle */}
